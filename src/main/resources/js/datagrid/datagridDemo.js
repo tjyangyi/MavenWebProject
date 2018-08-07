f70e40f1850ca40ad6fab6398e4c7f43bd276542
YangYi <368315960@qq.com>
2018-08-07 17:07:03 +0800

    修改静态文件位置

--- src/main/resources/js/datagrid/datagridDemo.js
+++
@@ -1,8 +0,0 @@
-function onSearchClick() {
-	$('#dg').datagrid('load', {
-		startTime : $('#startTime').val(),
-		endTime : $('#endTime').val(),
-		name : $('#name').val().trim(),
-		count : $('#count').val()
-	});
-}
--- src/main/resources/js/hbase/hbaseDemo.js
+++
@@ -1,9 +0,0 @@
-function scanWithStartAndStop() {
-	// 一般使用简单易用的高层实现 $.get, $.post等
-	// 大多数情况下你无需直接操作$.ajax函数，除非你需要操作不常用的选项，以获得更多的灵活性
-	$.post("scanWithStartAndStop", {
-	}, function(data, status) {
-		console.log(data);
-		console.log(status);
-	});
-}
